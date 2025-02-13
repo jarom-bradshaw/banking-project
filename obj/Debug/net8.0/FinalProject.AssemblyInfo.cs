b358e0dd5997fff28e98d1a0b241825371bf9ce7
jarom-bradshaw <156943555+jarom-bradshaw@users.noreply.github.com>
2025-02-12 20:02:46 -0700

    File Directory Changes

--- obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be9e59984da2c3c2874f58bc37cc79ae7a4c1322")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af52a90212204638ddb381f031212befc34a8694")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
